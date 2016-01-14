d1b119e9e619861bb3c8277dd3902e6c6e77d6af
Komal Rathi <komal.rathi@gmail.com>
2016-01-14 10:39:47 -0500

    cgp: adds refitcn to cgp

---
+++ cgp/refitcn.R
@@ -0,0 +1,44 @@
+# refitcn.R
+# Author: Komal Rathi, Gunes Gundem
+# Institute: Memorial Sloan Kettering Cancer Center
+# Created ##------ Thu Jan 14 10:13:54 2016 ------##
+# Last Modified ##------ Thu Jan 14 10:13:54 2016 ------##
+# Function: 
+# This script takes in the chromosome, position, expected Major and Minor allele copy numbers
+# It re-calculates rho and psi and re-fits them to get a new copy number profile
+
+# get chr, pos, major and minor allele
+TMPBBG <- Sys.getenv('TMPBBG') # tmpBattenberg
+TNAME <- Sys.getenv('TNAME') # tumor name
+OUTDIR <- Sys.getenv('OUTDIR') # output dir
+chr <- as.numeric(Sys.getenv('CHR')) # chr
+pos <- as.numeric(Sys.getenv('POS')) # pos
+ref_seg_nMajor <- as.numeric(Sys.getenv('MJA')) # major allele
+ref_seg_nMinor <- as.numeric(Sys.getenv('MNA')) # minor allele
+
+# get subclones.txt
+dat <- read.delim(paste(TMPBBG,'/',TNAME,'_subclones.txt',sep=''))
+
+# get BAF and LogR of the chr and position
+ref_seg_BAF <- dat[dat$chr==chr & dat$startpos==pos,'BAF']
+ref_seg_LogR <- dat[dat$chr==chr & dat$startpos==pos,'LogR']
+
+# run script to adjust rho and psi
+gamma_param = 1 # 0.55 for array data
+
+# get corrected rho and psi
+rho = (2*ref_seg_BAF - 1) / (2*ref_seg_BAF - ref_seg_BAF*(ref_seg_nMajor + ref_seg_nMinor) - 1 + ref_seg_nMajor)
+psi = (rho*(ref_seg_nMajor + ref_seg_nMinor) + 2 - 2*rho)/(2^(ref_seg_LogR/gamma_param))
+psi = (psi-2*(1-rho))/rho
+
+# read err file to get the command
+library(stringr)
+f <- readLines(paste(OUTDIR,'/err/bbg_fitcn.1.err',sep=''))
+cmd <- grep("Errors from command:",f,value=TRUE)
+cmd <- sub('Errors from command: ','',cmd)
+
+# add rho and psi to cmd
+cmd <- paste(cmd,rho,psi) 
+
+# call the command
+system(cmd)
