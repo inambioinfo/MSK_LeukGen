2bd5739c05da1f192b5ac605d901f357dc1e7ae5
Komal Rathi <komal.rathi@gmail.com>
2016-02-09 18:16:00 -0500

    pileup: adds v1.1 pileup script

--- pileup/mpileup_wrapper_v1.1.R
+++ pileup/mpileup_wrapper_v1.1.R
@@ -60,19 +60,19 @@ include_insertions <- FALSE
 include_deletions <- FALSE
 
 if(length(args) >= 4){
-	max_depth <- as.integer(args[4])
-	if(length(args) >= 5){
-		min_base_quality <- as.integer(args[5])
-		if(length(args) >= 6){
-			min_mapq <- as.integer(args[6])
-			if(length(args) >= 7){
-				include_deletions <- as.logical(args[7])
-				if(length(args) >= 8){
-					include_insertions <- as.logical(args[8])
-				}
-			}
-		}
-	}
+    max_depth <- as.integer(args[4])
+    if(length(args) >= 5){
+        min_base_quality <- as.integer(args[5])
+        if(length(args) >= 6){
+            min_mapq <- as.integer(args[6])
+            if(length(args) >= 7){
+                include_deletions <- as.logical(args[7])
+                if(length(args) >= 8){
+                    include_insertions <- as.logical(args[8])
+                }
+            }
+        }
+    }
 }
 
 # read positions file & bam files
@@ -133,15 +133,15 @@ if(ncol(positions_file) >= 3 && nrow(positions_file) == 1){
 
 # get pileup for each file
 for(i in files){
-	print(paste("Processing...", basename(i), sep = ''))
-	bamfile <- i
-	bf <- BamFile(bamfile)
-	param <- ScanBamParam(which = GRanges(positions_file$V1,
+    print(paste("Processing...", basename(i), sep = ''))
+    bamfile <- i
+    bf <- BamFile(bamfile)
+    param <- ScanBamParam(which = GRanges(positions_file$V1,
                           IRanges(start = as.numeric(positions_file$V2),
                                   end = as.numeric(positions_file$V2))))
 
-	# change max depth, strand specification, various cut-offs
-	p_param <- PileupParam(distinguish_strand = TRUE,
+    # change max depth, strand specification, various cut-offs
+    p_param <- PileupParam(distinguish_strand = TRUE,
                            distinguish_nucleotides = TRUE,
                            max_depth = max_depth,
                            include_deletions = include_deletions,
@@ -150,8 +150,8 @@ for(i in files){
                            min_mapq = min_mapq,
                            min_nucleotide_depth = 0)
 
-	# call pileup function
-	res <- pileup(bf, scanBamParam = param, pileupParam = p_param)
+    # call pileup function
+    res <- pileup(bf, scanBamParam = param, pileupParam = p_param)
 
     # if there is no pileup for this file, go to next
     # if(nrow(res) == 0) next
@@ -166,14 +166,14 @@ for(i in files){
         res <- res[,c(1,2,4,5,6,3)]
     }
 
-	# get reference base
-	res <- merge(res, positions_file, by.x = c('seqnames', 'pos'), by.y = c('V1', 'V2'),all.y=TRUE)
+    # get reference base
+    res <- merge(res, positions_file, by.x = c('seqnames', 'pos'), by.y = c('V1', 'V2'),all.y=TRUE)
     res$strand <- factor(res$strand,levels=c('-','+'))
     res$nucleotide <- factor(res$nucleotide,levels=c('A','T','G','C'))
     res$seqnames <- factor(res$seqnames, levels=unique(res$seqnames))
 
-	# process and write the output
-	results <- dcast(res, seqnames+pos~nucleotide+strand,
+    # process and write the output
+    results <- dcast(res, seqnames+pos~nucleotide+strand,
                      value.var = "count", fill = 0, drop=FALSE)
     results$REF <- positions_file$REF
     results <- results[,c(1,2,ncol(results),3:(ncol(results)-1))]
@@ -184,23 +184,23 @@ for(i in files){
     }
 
     # calculate depth
-	results$D <- apply(results[, 4:ncol(results)], 1, sum)
-	results$D_forward <- apply(results[, grep('[+]', colnames(results))],
+    results$D <- apply(results[, 4:ncol(results)], 1, sum)
+    results$D_forward <- apply(results[, grep('[+]', colnames(results))],
                                1, sum)
-	results$D_reverse <- apply(results[, grep('[-]', colnames(results))],
+    results$D_reverse <- apply(results[, grep('[-]', colnames(results))],
                                1, sum)
     results$TAG <- tags
-	results <- results[, c(1, 2, 3, ncol(results),
+    results <- results[, c(1, 2, 3, ncol(results),
                            grep('D', colnames(results)),
                            grep('[+]', colnames(results)),
                            grep('[-]', colnames(results)))]
-	colnames(results) <- sub('[+]', 'forward', colnames(results))
-	colnames(results) <- sub('[-]', 'reverse', colnames(results))
-	colnames(results)[1:2] <- c('CHR', 'POS')
+    colnames(results) <- sub('[+]', 'forward', colnames(results))
+    colnames(results) <- sub('[-]', 'reverse', colnames(results))
+    colnames(results)[1:2] <- c('CHR', 'POS')
 
-	# output generation
-	outfile <- sub('[.]bam', '.out', sub('.*/', '', i))
-	write.table(x = results, file = outfile, quote = F,
+    # output generation
+    outfile <- sub('[.]bam', '.out', sub('.*/', '', i))
+    write.table(x = results, file = outfile, quote = F,
                 row.names = F, sep = '\t')
 }
 
